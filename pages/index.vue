4621569649e2ae5bd717278b17b78ec71632505a
jochem <619velde@gmail.com>
2025-06-04 12:22:41 +0200

    minor routing fixes

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="p-4 space-y-4">
     <h1 class="text-xl font-bold">Welkom bij Potje Verkopen</h1>
-    <NuxtLink v-if="!spelBestaat" to="/spelen/spelen" class="btn">Nieuw spel</NuxtLink>
+    <NuxtLink v-if="!spelBestaat" to="/spelen" class="btn">Nieuw spel</NuxtLink>
     <div v-else class="space-y-2">
-      <NuxtLink to="/spelen/spelen" class="btn">Verder spelen</NuxtLink>
+      <NuxtLink to="/spelen" class="btn">Verder spelen</NuxtLink>
       <button class="btn" @click="opnieuw">Begin opnieuw</button>
     </div>
   </div>
--- pages/spelen/index.vue
+++ pages/spelen/index.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="p-4 space-y-4">
     <NuxtLink v-if="!spelBestaat" to="/spelen/spelers" class="btn">Spelers aanmaken</NuxtLink>
-    <NuxtLink v-if="!spelBestaat" to="/potje/potje" class="btn">Begin</NuxtLink>
+    <NuxtLink v-if="!spelBestaat" to="/potje" class="btn">Begin</NuxtLink>
     <NuxtLink v-else to="/spelen/spelers" class="btn">Bekijk spelers</NuxtLink>
-    <NuxtLink to="/potje/potje" class="btn">Verder spelen</NuxtLink>
+    <NuxtLink to="/potje" class="btn">Verder spelen</NuxtLink>
   </div>
 </template>
 <script setup lang="ts">

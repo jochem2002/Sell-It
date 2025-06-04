22a34425c0fa5a03a62ac0f87e7e60d63dd90daf
jochem <619velde@gmail.com>
2025-06-04 12:20:45 +0200

    Added theme colors, tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,9 +6,25 @@ export default <Partial<Config>>{
         './layouts/**/*.{vue,js,ts}',
         './pages/**/*.{vue,js,ts}',
         './app.vue',
+        './node_modules/@nuxthq/ui/**/*.{js,ts,vue}'
     ],
     theme: {
-        extend: {}
+        extend: {
+            colors: {
+                primary: {
+                    50: '#eff6ff',
+                    100: '#dbeafe',
+                    200: '#bfdbfe',
+                    300: '#93c5fd',
+                    400: '#60a5fa',
+                    500: '#3b82f6',
+                    600: '#2563eb',
+                    700: '#1d4ed8',
+                    800: '#1e40af',
+                    900: '#1e3a8a'
+                }
+            }
+        }
     },
     plugins: []
 }
